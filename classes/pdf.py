d714541747d96a42903b9b681fdc5fe881a04fa7
Serg <serg@example.com>
2024-04-16 09:48:36 +0200

    Первичная реализация класса pdf. Извлечение табличной части.

--- classes/pdf.py
+++ classes/pdf.py
@@ -12,7 +12,7 @@ class PDF:
         self.path_dir = path_dir
         self.pdf_file = pdf_file
 
-    def is_contains_data_table(self) -> bool:
+    def is_contains_data_table(self) :
         full_path = f'{self.path_dir}/{self.pdf_file}'
 
         for pagenum, page in enumerate(extract_pages(pdf_file=full_path)):
@@ -24,5 +24,19 @@ class PDF:
 
 
 pdf = PDF(path_dir='../extract_assets', pdf_file='BL24-10003.pdf')
-print(pdf.is_contains_data_table())
 
+if pdf.is_contains_data_table():
+    pdf_path = '../extract_assets/BL24-10003.pdf'
+
+    def extract_table(pdf_path, page_num, table_num):
+        pdf = pdfplumber.open(pdf_path)
+        # Найти исследуемую страницу
+        table_page = pdf.pages[page_num]
+        # Извлечение соответствующей таблицы
+        table = table_page.extract_tables()[table_num]
+        print(table, type(table), sep='\n')
+
+    extract_table(pdf_path, page_num=0, table_num=0)
+
+else:
+    print("Таблица не найдена.")
