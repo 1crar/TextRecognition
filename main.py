c4c839432030dc5d8a1495ad03b771e10429aa49
Сергей Федотов <fedser@lan.rarus.ru>
2024-06-21 18:09:28 +0300

    Переход на камелот. Добавил параметр line_scale=40 и это решило проблему с определением таблиц из pdf

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from log_config.log_config import activate_logging
 
 from classes.data_json import DataCollection, DataCleaning, DictToJson
-from classes.pdf import PdfAnalyzer, PdfTabula, PdfCamelot, PdfTextReader, extract_tables, dt_frame_to_list
+from classes.pdf import PdfAnalyzer, PdfTabula, PdfTextReader, PdfCamelot, dt_frame_to_list
 from classes.img import ImageDataExtracter, tesseract_languages
 from classes.text_extracter import InnInvoiceDataExtraction
 
@@ -40,26 +40,37 @@ if __name__ == '__main__':
     else:
         start = time.time()
         activate_logging()
+        pdf_analyzer = PdfAnalyzer(path_dir='extract_assets/input_files/upds_and_invoices',
+                                   pdf_file='УПД 31.05.24 № 428 = 257 428.00 без НДС.pdf')
+        pages = pdf_analyzer.get_pages()
+
         # Создаем экземпляр класса на основе либы камелот (для извлечения табличной части из pdf)
-        # camelot_instance = PdfCamelot(path_dir='extract_assets/input_files/upds_and_invoices',
-        #                               pdf_file='combinepdf.pdf')
-        tabula_instance = PdfTabula(path_dir='extract_assets/input_files/upds_and_invoices',
-                                    pdf_file='Счет-фактура и УПД 29.05.24 № 36989907 = 2 873.59 в т.ч. НДС 478.94.pdf')
+        camelot_instance = PdfCamelot(path_dir='extract_assets/input_files/upds_and_invoices',
+                                      pdf_file='Счет-фактура и УПД 29.05.24 № 36989907 = 2 873.59 в т.ч. НДС 478.94.pdf')
+
+        # tabula_instance = PdfTabula(path_dir='extract_assets/input_files/upds_and_invoices',
+        #                             pdf_file='УПД 31.05.24 № 428 = 257 428.00 без НДС.pdf')
+
         # Считываем таблицы с помощью камелота
-        # tables = camelot_instance.read_tables()
+        tables = camelot_instance.read_tables()
+
         # Считываем таблицы с помощью tabula
-        tables = tabula_instance.read_tables()
+        # tables = tabula_instance.read_tables()
+
         # Нас интересует только первая таблица (данные по товарам, camelot выделяет первым эту часть данных)
         # В теории, тут должен быть цикл обработки таблицы для каждой страницы (особенно важно для многостраничных pdf)
         # Получаем кол-во таблиц для обработки
         # table_numbers = tables.n
+
         # Преобразовываем dataframes в lists для дальнейшей обработки
-        table_lists = dt_frame_to_list(table_frame=tables)
-        logger.info("До преобразования: \n%s", tables)
-        logger.info("После преобразования: \n%s", table_lists)
-        # table: list = tables[0].data
+        # table_lists = dt_frame_to_list(table_frame=tables)
+
+        # logger.info("До преобразования: \n%s", tables)
+        # logger.info("После преобразования: \n%s\n%s", table_lists[0], table_lists[1])
+
+        table: list = tables[0].data
         # Затем импортируем класс DataCleaning для очистки и работы с извлеченной таблицей
-        cleaned_table: list = DataCleaning.data_clean(data_table=table_lists)
+        cleaned_table: list = DataCleaning.data_clean(data_table=table)
         logger.info('Очистка таблицы данных: \n%s', cleaned_table)
 
         # camelot_instance.write_to_csv(tables=tables, file_csv_name='Camelot_result.csv')
@@ -67,7 +78,7 @@ if __name__ == '__main__':
         # Далее извлекаем текст из pdf (без учета структуры) для извлечения данных вне табличной части
         # (ИНН/КПП, Счет-фактура)
         pdf = PdfTextReader(path_dir='extract_assets/input_files/upds_and_invoices',
-                            pdf_file='combinepdf.pdf')
+                            pdf_file='Передаточный документ 31.05.24 № 54503 = 2 191.99 без НДС.pdf')
         # Создаем экземпляр класса
         text = pdf.extract_text_from_pdf()
         # Извлекаем текст из pdf
