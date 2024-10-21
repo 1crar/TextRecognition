808718308cb74f3849b2af4d92866fc00277d258
Serhio <genFPS@mail.test>
2024-10-21 18:52:43 +0300

    Реализация метода from_png_to_pdf для класса Converter

--- app/pdf_appRecognizer/classes/converter.py
+++ app/pdf_appRecognizer/classes/converter.py
@@ -1,20 +1,40 @@
+import os
+
+from dotenv import load_dotenv
+from PIL import Image
 from pdf2image import convert_from_path
 
 
+load_dotenv()
+
+
 class Converter:
-    def __init__(self,):
+    # Указываем путь до папки bin\ становленной программы poppler
+    _poppler_path: str = os.getenv('POPPLER_PATH')
+
+    def __init__(self):
         """
         Скорее всего, также нужно будет указать путь до исполнительного файла, как и в классе Img
         def __init__(self, path_dir: str, exe_file: str):
             self.path_dir = path_dir
             self.exe_file = exe_file
         """
+
         pass
 
     @staticmethod
-    def convert_from_pdf_to_img(pdf_path: str, to_save: str) -> None:
+    def from_pdf_to_png(pdf_path: str, to_save: str) -> None:
         # Второй параметр влияет на качество изображения (при необходимости можно поставить выше значение)
-        pages = convert_from_path(pdf_path=pdf_path, dpi=200)
+        pages = convert_from_path(pdf_path=pdf_path, dpi=200, poppler_path=Converter._poppler_path)
         # Сохраняем каждое изображение в PNG формате
         for i, page in enumerate(pages):
             page.save(f'{to_save}_{i + 1}.png', 'PNG')
+
+    @staticmethod
+    def from_png_to_pdf(png_path: str, to_save: str) -> None:
+        pdf_file = Image.open(png_path)
+        pdf_file.convert('RGB')
+        # Сохраняем конфертированный из png в pdf файл
+        pdf_file.save(fp=to_save, quality=100)
+
+
