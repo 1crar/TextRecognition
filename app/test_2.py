b7fd2e48273ded922717f34dee82958158d51721
Serhio <genFPS@mail.test>
2024-12-02 18:31:17 +0300

    Небольшие изменения в app/test_2.py

--- app/test_2.py
+++ app/test_2.py
@@ -15,7 +15,7 @@ load_dotenv()
 
 
 enhanced_file: str = 'temp/scaled4_loveimg_test_2.png'
-image_path: str = f'pdf_appRecognizer/extract_assets/image_files/dt_cells/table_cell_0_3266_555.png'
+image_path: str = f'pdf_appRecognizer/extract_assets/image_files/dt_cells/table_cell_0_2133_555.png'
 
 image_cv2 = cv2.imread(filename=image_path)
 languages: list = ['ru']
@@ -26,17 +26,18 @@ languages: list = ['ru']
 print("[INFO] OCR'ing input image...")
 print(f"[INFO] OCR'ing with the following languages: {languages}")
 
-test_case: int = 2
+test_case: int = 1
 
 if test_case == 1:
-    reader = easyocr.Reader(lang_list=languages, gpu=True,
-                            model_storage_directory='fine_tune', user_network_directory='fine_tune',
-                            recog_network='ru_finetune')
+    # reader = easyocr.Reader(lang_list=languages, gpu=True,
+    #                         model_storage_directory='fine_tune', user_network_directory='fine_tune',
+    #                         recog_network='ru_finetune')
+    reader = easyocr.Reader(lang_list=languages, gpu=True)
     # results = reader.readtext(image_cv2, text_threshold=0.75, contrast_ths=0.7, width_ths=1.25, height_ths=0.75,
     #                           ycenter_ths=0.5, slope_ths=1, add_margin=0.175, decoder='wordbeamsearch', beamWidth=20,
     #                           canvas_size=3500)
     # Not bad
-    results = reader.readtext(image_cv2, text_threshold=0.75, contrast_ths=0.8, width_ths=1.3, height_ths=0.75,
+    results = reader.readtext(image_cv2, text_threshold=0.7, contrast_ths=0.8, width_ths=1.2, height_ths=0.8,
                               ycenter_ths=0.5, slope_ths=1, add_margin=0.200, decoder='wordbeamsearch', beamWidth=20,
                               canvas_size=3500)
 if test_case == 2:
@@ -49,7 +50,7 @@ image_pil = Image.fromarray(cv2.cvtColor(image_cv2, cv2.COLOR_BGR2RGB))
 draw = ImageDraw.Draw(image_pil)
 
 # Выберите шрифт (если нужный шрифт не установлен, вы можете указать путь к .ttf файлу)
-font = ImageFont.truetype(font="arial.ttf", size=40)  # Убедитесь, что путь к шрифту корректный
+font = ImageFont.truetype(font="arial.ttf", size=20)  # Убедитесь, что путь к шрифту корректный
 
 for (bbox, text, prob) in results:
     print("[INFO] {:.4f}: {}".format(prob, text))
