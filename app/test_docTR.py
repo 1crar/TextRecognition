17732a7f6a182d9ae057d5acb9be416c86d8aad6
Serhio <genFPS@mail.test>
2025-01-09 15:18:14 +0300

    Небольшие изменения в app/test_docTR.py

--- app/test_docTR.py
+++ app/test_docTR.py
@@ -5,11 +5,15 @@ from doctr.io import DocumentFile
 from doctr.datasets import VOCABS
 
 
-img_path: str = 'pdf_appRecognizer/extract_assets/image_files/YPD_1/upscaled_4_UPD_1_dt_part.png'
+img_path: str = 'pdf_appRecognizer/extract_assets/image_files/YPD_1/UPD_1.png'
 local_model_path: str = 'trained_models/docTR/crnn_vgg16_bn_20241226-154828.pt'
+# Подключаем gpu если torch.cuda.is_available() - True
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 
 own_model = crnn_vgg16_bn(pretrained=False, pretrained_backbone=False, vocab=VOCABS["ukrainian"])
-model_params = torch.load(f=local_model_path, map_location="cpu")
+
+model_params = torch.load(f=local_model_path, map_location=device)
 own_model.load_state_dict(model_params)
 
 predictor = ocr_predictor(det_arch='linknet_resnet18', reco_arch=own_model, pretrained=True)
@@ -23,7 +27,7 @@ for text in text_blocks:
     # print(text.lines)
     for el in text.lines:
         # Вывод извлеченного текста с помощью docTR
-        print(f'Распознанный текст: {el.words}')
+        print(f'Распознанный текст/элементы текста: {el.words}')
 
 
 result.show()
